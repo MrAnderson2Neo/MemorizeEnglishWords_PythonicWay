98499ede150c8c6c2656b06cce549071b98bf222
CaryLeo <uniqueasleo@gmail.com>
2017-05-07 08:58:36 +0800

    创建了words.json文件

--- src/analyseWords.py
+++ src/analyseWords.py
@@ -9,8 +9,10 @@ with open("articles.txt","r") as f:
 
 sortedWords = sorted(words.items(),key = lambda e : e[1],reverse=True )
 print(sortedWords)
-for pair in sortedWords:
-    with open("words.txt","a+") as f:
-        f.write(pair[0]) 
-        f.write("\n")
+with open("words.json","w") as f:
+    f.write(str(dict(sortedWords)))
+# for pair in sortedWords:
+#     with open("words.txt","a+") as f:
+#         f.write(pair[0]) 
+#         f.write("\n")
     
